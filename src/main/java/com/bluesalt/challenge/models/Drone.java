7e3701c52107224c7a4f5453bd79a21edc9a5f90
Joel Eze <joelezeu@gmail.com>
2023-10-09 16:19:26 +0100

    Added Spring Data JPA lib

--- src/main/java/com/bluesalt/challenge/models/Drone.java
+++ src/main/java/com/bluesalt/challenge/models/Drone.java
@@ -1,5 +1,19 @@
 package com.bluesalt.challenge.models;
 
-public class Drone {
 
+import java.util.List;
+
+public class Drone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String serialNumber;
+    private String model;
+    private double weightLimit;
+    private int batteryCapacity;
+    private String state;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "drone")
+    private List<Medication> loadedMedications;
 }
--- src/main/java/com/bluesalt/challenge/models/Medication.java
+++ src/main/java/com/bluesalt/challenge/models/Medication.java
@@ -1,5 +1,16 @@
 package com.bluesalt.challenge.models;
 
 public class Medication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+    private double weight;
+    private String code;
+    private String image;
+
+    @ManyToOne
+    @JoinColumn(name = "drone_id")
+    private Drone drone;
 }
