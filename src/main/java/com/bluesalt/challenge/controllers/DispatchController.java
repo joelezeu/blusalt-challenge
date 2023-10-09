ce3beb0813cef6b925381f09070b1efa24a4032b
Joel Eze <joelezeu@gmail.com>
2023-10-09 16:47:44 +0100

    Register the drone to a controller.

--- src/main/java/com/bluesalt/challenge/controllers/DispatchController.java
+++ src/main/java/com/bluesalt/challenge/controllers/DispatchController.java
@@ -1,9 +1,24 @@
 package com.bluesalt.challenge.controllers;
 
+import com.bluesalt.challenge.domain.DroneRequest;
+import com.bluesalt.challenge.services.DispatchService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@RequiredArgsConstructor
 public class DispatchController {
+
+    private final DispatchService dispatchService;
+
+    // Register a Drone
+    @PostMapping("/drones")
+    public ResponseEntity<?> registerDrone(@RequestBody DroneRequest drone) {
+       return dispatchService.registerDrone(drone);
+    }
 }
