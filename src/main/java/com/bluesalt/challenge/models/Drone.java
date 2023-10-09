6bbf608a2028b935c4186da20467585c933e3c2d
Joel Eze <joelezeu@gmail.com>
2023-10-09 16:24:20 +0100

    Created Models for Dispatch Service

--- src/main/java/com/bluesalt/challenge/models/Drone.java
+++ src/main/java/com/bluesalt/challenge/models/Drone.java
@@ -1,8 +1,18 @@
 package com.bluesalt.challenge.models;
 
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class Drone {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,5 +25,22 @@ public class Drone {
     private String state;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "drone")
+    @ToString.Exclude
     private List<Medication> loadedMedications;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Drone drone = (Drone) o;
+        return getId() != null && Objects.equals(getId(), drone.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/bluesalt/challenge/models/Medication.java
+++ src/main/java/com/bluesalt/challenge/models/Medication.java
@@ -1,16 +1,43 @@
 package com.bluesalt.challenge.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
 public class Medication {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private double weight;
+    private Double weight;
     private String code;
     private String image;
 
     @ManyToOne
     @JoinColumn(name = "drone_id")
     private Drone drone;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Medication that = (Medication) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
